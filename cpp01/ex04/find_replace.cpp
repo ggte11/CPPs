171a12cc89e88327ba329511adcf57318b011f05
Martim Cardoso <mcardoso@c1r10s3.42lisboa.com>
2026-07-02 03:39:07 +0100

    fixed ex04 to be able to replace the whole file insted of only the line

--- cpp01/ex04/find_replace.cpp
+++ cpp01/ex04/find_replace.cpp
@@ -2,17 +2,19 @@
 
 void find_replace(std::fstream &file, std::fstream &new_file, std::string to_find, std::string to_replace) {
 	if (to_find.empty())
-		return ;
+		return;
+	std::string content;
 	std::string line;
 	while (std::getline(file, line)) {
-		std::string::size_type pos = 0;
-		while ((pos = line.find(to_find, pos)) != std::string::npos) {
-			line.erase(pos, to_find.length());
-			line.insert(pos, to_replace);
-			pos += to_replace.length();
-		}
-		new_file << line;
+		content += line;
 		if (!file.eof())
-			new_file << '\n';
+			content += '\n';
 	}
+	std::string::size_type pos = 0;
+	while ((pos = content.find(to_find, pos)) != std::string::npos) {
+		content.erase(pos, to_find.length());
+		content.insert(pos, to_replace);
+		pos += to_replace.length();
+	}
+	new_file << content;
 }
